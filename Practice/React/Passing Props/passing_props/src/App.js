55f61846a216eb8e78f75426b7063c8c352f2d54
NethanT4 <nethant4@gmail.com>
2026-07-31 13:41:06 +0400

    learning to pass props in react

---
+++ Practice/React/Passing Props/passing_props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstname="Bob" />
+            <Heading firstname="Pete" />
+        </div>
+    );
+};
+
+export default App;
---
+++ Practice/React/Passing Props/passing_props/src/Heading.js
@@ -0,0 +1,6 @@
+function Heading(props) {
+    return (
+        <h1>Hello, {props.firstname}</h1>
+    )
+}
+export default Heading;
